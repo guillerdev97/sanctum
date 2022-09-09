f857b86f00f8006a62607794d25738455c18f449
Guillermo García <guillermieres97@gmail.com>
2022-09-09 23:55:47 +0200

    feat: implementation of infouser endpoint api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,5 +47,7 @@ class AuthController extends Controller
         ]);
     }
 
-    
+    public function infoUser(Request $request) {
+        return $request->user();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/infouser', [AuthController::class, 'infoUser'])->middleware('auth:sanctum');
